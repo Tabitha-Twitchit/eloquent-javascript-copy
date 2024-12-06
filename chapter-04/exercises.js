a7f15a60901e61bfb92adc8fc8d6daff279c9335
Tabitha-Twitchit <tabitha@tabithanikolai.com>
2024-12-06 13:10:13 -0600

    Complete ES 4

--- chapter-04/exercises.js
+++ chapter-04/exercises.js
@@ -2,7 +2,31 @@
 // range ///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function range() {
+function range(start, end, step) {
+//  check if equal off the bat and return empty array if so
+  if (start === end || step < 0){
+    return [];
+  } 
+
+  if(step === undefined){
+    let output = [start];
+    // console.log(output);
+    for (let i = 1; i < end; i++ ){
+      start +=1
+      output[i] = start;
+      // console.log(output);
+    }
+    return output;
+  } else {
+    let output = [start];
+    // console.log(output);
+    for (let i = 1; start < end; i++ ){
+      start +=step
+      output[i] = start;
+      // console.log(output);
+    }
+    return output;
+  }
 
 }
 
@@ -10,64 +34,167 @@ function range() {
 // sum /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function sum() {
+function sum(array, total=0) {
+  if(array.length === 0){
+    return total;
+  }
 
+  total += array[0];
+
+  return sum(array.slice(1), total);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // reverseArray ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function reverseArray() {
+function reverseArray(array) {
+  outputArray = [];
 
+  for (let i = 0; i < array.length; i++){
+    outputArray.unshift(array[i]);
+  }
+  return outputArray;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // reverseArrayInPlace /////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function reverseArrayInPlace() {
+function reverseArrayInPlace(array) {
 
+  for(let i = 0; i < Math.floor(array.length /2); i++){
+    let tempElem = array[i];
+    array[i] = array[array.length -1 -i];
+    array[array.length -1 -i] = tempElem;
+  }
+return array;
+
+// TESTS DIDN"T LIKE THIS VIABLE SOLUTION
+  //   tempArray = [];
+//   for (let i = 0; i < array.length; i++){
+//     tempArray.unshift(array[i]);
+//   }
+//   console.log("Newly loaded temArray: " + tempArray);
+//   console.log("Post 1st loop array: "+ array);
+
+//   array = [];
+//   console.log("Post clear array: "+ array);
+//   for (let i = 0; i < tempArray.length; i++){
+//     array.push(tempArray[i]);
+//   }
+//   console.log("newly reloaded array: " + array);
+//  return array;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // arrayToList /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function arrayToList() {
+function arrayToList(array) {
+  let rest = null;
 
+  for (let i = array.length -1; i >= 0; i--){
+    rest = {value: array[i], rest: rest}
+  }
+  return rest;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // listToArray /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function listToArray() {
-
+function listToArray(list, array=[]) {
+// base
+if(list.rest === null) {
+  array.push(list.value);
+  return array;
+}
+// recursion
+array.push(list.value);
+return listToArray(list.rest, array);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // prepend /////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function prepend() {
+/*Add the helper functions prepend, which takes an element and a list 
+and creates a new list that adds the element to the front of the input
+list,
+*/
+function prepend(element, list) {
+  // console.log(list);
+  // console.log(element);
 
+  // decompile the list into an array
+let tempArray = listToArray(list);
+// console.log(tempArray);
+
+// insert the new element at the beginning of the array
+tempArray.unshift(element);
+// console.log(tempArray);
+
+// recompile the list
+list = arrayToList(tempArray);
+return list;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // nth /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function nth() {
+/*nth, which takes a list and a number and returns the element at the 
+given position in the list (with zero referring to the first element) 
+or undefined when there is no such element.
+*/
 
+function nth(list, outputInd) {
+  console.log(outputInd);
+  
+  // decompile the list
+  tempArray = listToArray(list);
+  console.log(tempArray);
+  
+  // check if the array includes an element at the given index
+  // If the index is less than the array, it's there (you don't)
+  // need to see the exact one until you access it. 
+  if(outputInd < tempArray.length){
+    // console.log(tempArray[outputElem])
+    return tempArray[outputInd];
+    // else return undefined
+  } else {
+    return undefined;
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // deepEqual ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function deepEqual() {
+function deepEqual(x, y) {
+//  check if they're something besides objs
+  if(typeof x !== "object" && typeof y !== "object"){
+  return x === y;
+ }
+// if one OR the other isn't an object
+ if (typeof x !== "object" || typeof y !== "object"){
+  return false;
+ }
 
+ let xKeys = Object.keys(x);
+ let yKeys = Object.keys(y);
+ if(xKeys.length !== yKeys.length){
+  return false;
+ }
+
+ for (let i = 0; i < xKeys.length; i++){
+  if(!yKeys.includes(xKeys[i]) || !deepEqual(x[xKeys[i]], y[xKeys[i]])){
+    return false;
+  }
+ }
+
+ return true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
